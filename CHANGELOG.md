a602a481cd02bb0b31e0ff875c531fb5c7364963
Brandon Bridges <brandonrbridges@outlook.com>
2023-11-28 14:17:16 +0000

    Formatting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,13 @@
 # Change Log
 
-All notable changes to the "next-helper" extension will be documented in this file.
+Notable changes to the project will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## 0.0.1 - 28th November 2023
+
+### Added
 
 - Initial release
+- Create Component command
---
+++ src/commands/createComponent.ts
@@ -0,0 +1,65 @@
+// VS Code
+import * as vscode from 'vscode'
+
+// Node
+import fs from 'fs'
+import path from 'path'
+
+// Helpers
+import {
+	findComponentDirectory,
+	generateComponentContent,
+	generateIndexContent,
+} from '../helpers'
+
+export const createComponentCommand = vscode.commands.registerCommand(
+	'nextjs-essentials.createComponent',
+	async () => {
+		const name = await vscode.window.showInputBox({
+			placeHolder: 'Component name',
+			prompt: 'Enter component name',
+			validateInput: (text) => {
+				return text && /^[A-Z][A-Za-z]*$/.test(text)
+					? null
+					: 'Component name must be in PascalCase'
+			},
+		})
+
+		if (!name) {
+			vscode.window.showErrorMessage('Component creation cancelled.')
+			return
+		}
+
+		const folders = vscode.workspace.workspaceFolders
+
+		if (!folders) {
+			vscode.window.showErrorMessage('No workspace folder found.')
+			return
+		}
+
+		const root = folders[0].uri.fsPath
+		const directory = await findComponentDirectory(root)
+
+		if (!directory) {
+			vscode.window.showErrorMessage('No components directory found.')
+			return
+		}
+
+		const componentFile = generateComponentContent(name)
+		const componentFilePath = path.join(directory, `/${name}/${name}.tsx`)
+
+		const indexFile = generateIndexContent(name)
+		const indexFilePath = path.join(directory, `/${name}/index.ts`)
+
+		fs.mkdirSync(path.join(directory, `/${name}`))
+
+		fs.writeFileSync(componentFilePath, componentFile)
+		fs.writeFileSync(indexFilePath, indexFile)
+
+		const openPath = vscode.Uri.file(componentFilePath)
+
+		vscode.workspace.openTextDocument(openPath).then((doc) => {
+			vscode.window.showTextDocument(doc)
+		})
+	}
+)
